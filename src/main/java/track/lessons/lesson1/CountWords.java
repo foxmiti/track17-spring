aea49e2e9f2884360354f3203dfa4e61244ac63b
foxmiti <Mitrikasanton@gmail.com>
2017-03-10 00:10:50 +0300

    SecondTry

--- src/main/java/track/lessons/lesson1/CountWords.java
+++ src/main/java/track/lessons/lesson1/CountWords.java
@@ -4,22 +4,21 @@ import java.io.*;
 
 /**
  * Задание 1: Реализовать два метода
- *
+ * <p>
  * Формат файла: текстовый, на каждой его строке есть (или/или)
  * - целое число (int)
  * - текстовая строка
  * - пустая строка (пробелы)
- *
- *
+ * <p>
+ * <p>
  * Пример файла - words.txt в корне проекта
- *
+ * <p>
  * ******************************************************************************************
- *  Пожалуйста, не меняйте сигнатуры методов! (название, аргументы, возвращаемое значение)
- *
- *  Можно дописывать новый код - вспомогательные методы, конструкторы, поля
- *
+ * Пожалуйста, не меняйте сигнатуры методов! (название, аргументы, возвращаемое значение)
+ * <p>
+ * Можно дописывать новый код - вспомогательные методы, конструкторы, поля
+ * <p>
  * ******************************************************************************************
- *
  */
 public class CountWords {
 
@@ -27,56 +26,58 @@ public class CountWords {
      * Метод на вход принимает объект File, изначально сумма = 0
      * Нужно пройти по всем строкам файла, и если в строке стоит целое число,
      * то надо добавить это число к сумме
+     *
      * @param file - файл с данными
      * @return - целое число - сумма всех чисел из файла
      */
     int intAnswer; // default answer
+
     public long countNumbers(File file) throws Exception {
         FileReader fr = new FileReader(file); // load file in reader
         BufferedReader reader = new BufferedReader(fr); // read string in buffer
         String line = reader.readLine(); // load string in line
-        while((line != null)){ // if line is null end circle and return 0
-            if(line.isEmpty()){  // if line ecuals empty run this
+        while ((line != null)) { // if line is null end circle and return 0
+            if (line.isEmpty()) {  // if line ecuals empty run this
                 line = reader.readLine(); // take new string in line
                 continue; // next count of circle
             }
             try { // if line take integer
                 intAnswer += Integer.parseInt(line); // sub line with answer
-            }  catch(NumberFormatException e){ // when line not integer
+            } catch (NumberFormatException e) { // when line not integer
                 line = reader.readLine(); // take new string line
                 continue; // next count of circle
             }
-         line = reader.readLine(); // take new string line
+            line = reader.readLine(); // take new string line
         }
         return intAnswer; // return answer
-        }
-
-
+    }
 
 
     /**
      * Метод на вход принимает объект File, изначально результат= ""
      * Нужно пройти по всем строкам файла, и если в строка не пустая и не число
      * то надо присоединить ее к результату через пробел
+     *
      * @param file - файл с данными
      * @return - результирующая строка
      */
     String lineAnswer = ""; // default answer
+
     public String concatWords(File file) throws Exception {
         FileReader fr = new FileReader(file); // load file in reader
         BufferedReader reader = new BufferedReader(fr); // read string in buffer
         String line = reader.readLine(); // load string in line
-        while((line != null)){ // if line is null end circle and return 0
-           // System.out.println(line);  // debug
-           // System.out.println(lineAnswer); // debug
-            if(line.isEmpty()){  // if line equal empty run this
+        while ((line != null)) { // if line is null end circle and return 0
+            // System.out.println(line);  // debug
+            // System.out.println(lineAnswer); // debug
+            if (line.isEmpty()) {  // if line equal empty run this
                 line = reader.readLine(); // take new string in line
                 continue; // next count of circle
-             }
+            }
             try { // if line take integer
                 Integer.parseInt(line); //
-            }  catch(NumberFormatException e){ // catch exception
-                lineAnswer = lineAnswer + " " + line ; // add words in line and add space firstly
+            } catch (NumberFormatException e) { // catch exception
+                lineAnswer = lineAnswer + " " + line; // add words in line and add space firstly
                 //line = reader.readLine();
                 //continue;
             }
